786f47bc2f40626d92cbbe9c74fea8850feb3805
M.C <hanhchaumn@gmail.com>
2021-07-10 17:13:34 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+"use strict";
+
+import { libWrapper } from "./lib/libwrapper.js";
+import { ModuleSettings, ModuleOptions } from "./scripts/settings.js";
+
+Hooks.on("setup", () => {
+	ModuleSettings.registerSettings();
+
+	libWrapper.register("bugmacro", "KeyboardManager.prototype._onDigit", function (wrapped, ...args) {
+		if (!ModuleSettings.getSetting(ModuleOptions.DISABLE_NUMBER)) {
+			wrapped(...args);
+		}
+	});
+});
---
+++ scripts/settings.js
@@ -0,0 +1,52 @@
+export const ModuleOptions = {
+	DISABLE_NUMBER: "disable_number"
+};
+
+const OnChangeFunctions = {
+	NOTHING: () => {},
+	RELOAD: () => window.location.reload()
+};
+
+export class ModuleSettings {
+	static MODULE_NAME = "bugmacro";
+
+	static registerSettings() {
+		game.settings.register(
+			this.MODULE_NAME,
+			ModuleOptions.DISABLE_NUMBER,
+			this._buildConfig(ModuleOptions.DISABLE_NUMBER, {
+				default: true,
+				scope: "world",
+				onChange: OnChangeFunctions.NOTHING
+			})
+		);
+	}
+
+	static getSetting(option) {
+		return game.settings.get(this.MODULE_NAME, option);
+	}
+
+	/** @private */
+	static _getNameConfig(optionName) {
+		return {
+			name: `${this.MODULE_NAME}.${optionName}-s`,
+			hint: `${this.MODULE_NAME}.${optionName}-l`
+		};
+	}
+
+	/** @private */
+	static _buildConfig(optionName, config = {}) {
+		const defaultConfig = {
+			scope: "client",
+			config: true,
+			default: false,
+			type: Boolean,
+			onChange: OnChangeFunctions.RELOAD
+		};
+		return {
+			...defaultConfig,
+			...this._getNameConfig(optionName),
+			...config
+		};
+	}
+}
